af74e0d5414f4f41af732b4b8befd211b038653d
soph57 <sophie-rossignol@sfr.fr>
2018-01-22 20:58:52 +0100

    routes abonnements header footer

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -21,5 +21,36 @@ class RouteController extends Controller
     public function registration()
     {
         return $this->render('registration.html.twig');
+
+    }
+    /**
+     * @Route("/abonnements", name="abonnements")
+     */
+    public function abonnements()
+    {
+        return $this->render('abonnements.html.twig');
+    }
+
+    /**
+     * @Route("/header", name="header")
+     */
+    public function header()
+    {
+        return $this->render('header.html.twig');
+
+    }
+    /**
+     * @Route("/footer", name="footer")
+     */
+    public function footer()
+    {
+        return $this->render('footer.html.twig');
+    }
+    /**
+     * @Route("/sidebar", name="sidebar")
+     */
+    public function sidebar()
+    {
+        return $this->render('sidebar.html.twig');
     }
 }
