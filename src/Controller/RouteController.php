64a431df5e2e7c8f2de166b5084aa8022801ff3f
Alexandre THUILLIER <alexandrethuillier@icloud.com>
2018-01-23 14:01:40 +0100

    Création des autres routes, liens vers les pages, corrections des CSS

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -1,56 +1,83 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Component\HttpFoundation\Response;
 
-class RouteController extends Controller
-{
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login()
+    class RouteController extends Controller
     {
-        return $this->render('login.html.twig');
-    }
-    /**
-     * @Route("/registration", name="registration")
-     */
-    public function registration()
-    {
-        return $this->render('registration.html.twig');
+
+        /**
+         * @Route("/", name="index")
+         */
+        public function index()
+        {
+            return $this->render('index.html.twig');
+        }
+
+
+        /**
+         * @Route("/login", name="login")
+         */
+        public function login()
+        {
+            return $this->render('login.html.twig');
+        }
+
+        /**
+         * @Route("/registration", name="registration")
+         */
+        public function registration()
+        {
+            return $this->render('registration.html.twig');
+
+        }
+
+        /**
+         * @Route("/abonnements", name="abonnements")
+         */
+        public function abonnements()
+        {
+            return $this->render('abonnements.html.twig');
+        }
+
+        /**
+         * @Route("/header", name="header")
+         */
+        public function header()
+        {
+            return $this->render('header.html.twig');
+
+        }
+
+        /**
+         * @Route("/footer", name="footer")
+         */
+        public function footer()
+        {
+            return $this->render('footer.html.twig');
+        }
+
+        /**
+         * @Route("/sidebar", name="sidebar")
+         */
+        public function sidebar()
+        {
+            return $this->render('sidebar.html.twig');
+        }
+
+        /**
+         * @Route("/company", name="company")
+         */
+        public function company()
+        {
+            return $this->render('company.html.twig');
+        }
+
+
+
+
 
     }
-    /**
-     * @Route("/abonnements", name="abonnements")
-     */
-    public function abonnements()
-    {
-        return $this->render('abonnements.html.twig');
-    }
-
-    /**
-     * @Route("/header", name="header")
-     */
-    public function header()
-    {
-        return $this->render('header.html.twig');
-
-    }
-    /**
-     * @Route("/footer", name="footer")
-     */
-    public function footer()
-    {
-        return $this->render('footer.html.twig');
-    }
-    /**
-     * @Route("/sidebar", name="sidebar")
-     */
-    public function sidebar()
-    {
-        return $this->render('sidebar.html.twig');
-    }
-}
